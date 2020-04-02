f8bb05b97aff2c2d4817686f0f41ab43be36cd49
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-04-02 19:51:56 -0400

    test again

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 
-console.log("test");
+console.log("testinggg");
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
